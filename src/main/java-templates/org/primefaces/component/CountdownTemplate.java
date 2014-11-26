7b3815eaf0e671d14e04d563016f39c6c43d7923
Batuhan Çıkrıkcı <batuhancikrikci@gmail.com>
2014-11-26 14:40:52 +0200

    added CountdownRenderer encodeScript method added template, css, js

---
+++ src/main/java-templates/org/primefaces/component/CountdownTemplate.java
@@ -0,0 +1,1 @@
+    public final static String STYLE_CLASS = "ui-countdown ui-widget ui-widget-header ui-corner-all";
--- src/main/java/org/primefaces/component/countdown/CountdownRenderer.java
+++ src/main/java/org/primefaces/component/countdown/CountdownRenderer.java
@@ -19,33 +19,51 @@ import java.io.IOException;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
+import org.primefaces.renderkit.CoreRenderer;
+import org.primefaces.util.WidgetBuilder;
 
 /**
  *
  * @author akursat
  * @author batuhancikrikci
  */
-public class CountdownRenderer {
-    
+public class CountdownRenderer extends CoreRenderer {
+
+    @Override
+    public void decode(FacesContext context, UIComponent component) {
+        Countdown countdown = (Countdown) component;
+
+    }
+
     public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
         Countdown countdown = (Countdown) component;
-        
+
         encodeMarkup(context, countdown);
         encodeScript(context, countdown);
     }
-    
-    protected void encodeScript(FacesContext context, Countdown countdown) throws IOException {
-        ResponseWriter writer = context.getResponseWriter();
-        
-        writer.startElement("span", countdown);
-        writer.writeAttribute("id", countdown.getClientId(), null);
-       // writer.writeAttribute("class", Countdown.STYLE_CLASS, null);
-        writer.endElement("span");
-        
-    }
 
     protected void encodeMarkup(FacesContext context, Countdown countdown) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+
+        writer.startElement("span", countdown);
+        writer.writeAttribute("id", countdown.getClientId(), null);
+        writer.writeAttribute("class", Countdown.STYLE_CLASS, null);
+        writer.endElement("span");
 
     }
 
+    protected void encodeScript(FacesContext context, Countdown countdown) throws IOException {
+        String clientId = countdown.getClientId(context);
+        WidgetBuilder wb = getWidgetBuilder(context);
+        String finishTime = countdown.getFinishTime();
+
+        wb.init("Countdown", countdown.resolveWidgetVar(), clientId);
+        wb.attr("pattern", countdown.getPattern(), null)
+                .attr("reverse", countdown.isReverse(), false)
+                .attr("finishTime", finishTime, "infinite")
+                .attr("autoStart", countdown.isAutostart(), false)
+                .attr("locale", context.getViewRoot().getLocale().toString());
+        
+
+    }
 }
